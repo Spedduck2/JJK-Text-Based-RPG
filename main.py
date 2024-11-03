685b0e916d30befd45cc96f011f9bc8bb2ee6b34
alexandros3015 <alexandrosbeachtoys@gmail.com>
2024-11-03 13:50:56 -0500

    Remove useless comments

--- main.py
+++ main.py
@@ -7,31 +7,33 @@ def check_input(text, *args):
     while True:
         inp = input(text)
         if inp in args:
-            return inp  # Exit the loop and return the valid input
+            return inp  
         else:
             print("Please enter a valid option.")
 
-# Define locations and options
 rareitem = ["Playful Cloud", "Split Soul Katana", "Black Rope", "Inverted Spear of Heaven"]
 item = ["Katana", "Spear", "Slaughter Demon"] * 3
 cs = ["a Grade 4 CS", "a Grade 3 CS", "a Grade 2 CS", "a Grade 1 CS"] * 5
 sgcs = ["Mahito", "Jogo", "Finger Bearer", "Hanami", "Dagon"]
+
 eyes = ["1"] * 7
 eyes2 = ["2"]
-# Randomly choose an item and location
+
 randit = random.choice(item + rareitem)
 loc = random.choice(["a boat in the middle of the ocean"])
 six = random.choice(eyes + eyes2)
-# Define options for CT
+
 rareoptions = ["Soul Manipulation", "Ten Shadows", "Shrine", "Limitless", "Heavenly Restriction"]
 options = ["Blessed By the Sparks", "Gambling", "Blood Manipulation", "Inverse", "Boogie Woogie", "Construction", "CE Heavenly Restriction", "Ratio"] * 2
 
-# Select two random options for CT
+
+
+
 cur1 = random.choice(options)
-options.remove(cur1)  # Remove chosen option to ensure uniqueness
+options.remove(cur1)  
 cur2 = random.choice(options)
 
-# Get user choice for CT
+
 chr = check_input(f"CHOOSE YOUR CT, {cur1} or {cur2} (1 or 2): \n", "1", "2")
 cur = cur1 if chr == "1" else cur2
 print(f"\n{cur} chosen\n")
@@ -39,7 +41,7 @@ if six == "2":
     print("you have been given the six eyes")
 print(f"You wake up in {loc}")
 
-# Check action input
+
 act = check_input("What do you do now? 1 = look around, 2 = Explore: ", "1", "2")
 
 if act == "1":
@@ -47,10 +49,10 @@ if act == "1":
 elif act == "2":
     if loc == "a boat in the middle of the ocean":
         print(f"\nYou go swimming and find {randit}.")
-        csr = random.choice(cs + sgcs)  # Concatenate lists and choose one
+        csr = random.choice(cs + sgcs)  
         print(f"Out of breath, you go back to the boat, but you find {csr}.")
         
-        # Check if csr is in the list of strong cursed spirits
+        
         if csr in sgcs:
             print("Yup, you get eviscerated.")
             print("End of journey.")
@@ -62,5 +64,4 @@ elif act == "2":
                 print("Your rare item helped you defeat the CS with minimal injuries.")
             elif randit in item:
                 print("You win but suffer major injuries.")
-                
-# Here you can add more game mechanics, story elements, or battle systems!
+
